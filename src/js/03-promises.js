84d9742d722652c9654b55d2db8949f716418b56
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-07-30 10:00:07 +0300

    task 3 finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+const formRef = document.querySelector('.form');
+const delayEl = document.querySelector('[name="delay"]');
+const stepEl = document.querySelector('[name="step"]');
+const amountEl = document.querySelector('[name="amount"]');
+
+formRef.addEventListener('submit', onSubmitCreatePromise);
+
+function onSubmitCreatePromise(event) {
+  event.preventDefault();
+
+  let timeOut = Number(delayEl.value);
+  const step = Number(stepEl.value);
+  const amount = Number(amountEl.value);
+
+  for (i = 1; i <= amount; i += 1){
+    timeOut += step;
+
+    createPromise(i, timeOut)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  })
 }
